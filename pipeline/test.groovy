782b3eb3b6715cc0908a5b021eff17be8a331ac2
roms <roms@romsnet.net>
2021-10-30 19:57:52 +0200

    add redis container

--- pipeline/test.groovy
+++ pipeline/test.groovy
@@ -1,9 +1,15 @@
 def call() {
   stage("test") {
     sh """
-      docker run --rm -d -p 5500:5500 -e PORT=5500 --name=shorturl_worker shorturl/worker:staging
-      #curl -v localhost:5500
-      docker stop shorturl_worker
+      docker run --rm -d -p 5500:5500 -e PORT=5500 --name=shorturl_worker_${BRANCH_NAME} raspi01:5000/shorturl/worker:staging
+      shorturl_worker_ip=\$(docker exec shorturl_worker_${BRANCH_NAME} hostname -i)
+      echo
+      echo "### logs ###"
+      docker logs shorturl_worker_${BRANCH_NAME}
+      echo
+      echo "### curl ###"
+      curl -v \${shorturl_worker_ip}:5500
+      docker stop shorturl_worker_${BRANCH_NAME}
     """
   }
 }
