9d02de233d09d374498ea8e6f6594b6b29e55a2f
roms <roms@romsnet.net>
2021-11-02 15:13:42 +0100

    reactivate arm build

--- pipeline/main.groovy
+++ pipeline/main.groovy
@@ -8,24 +8,8 @@ def call() {
   stash name: "repo"
 
   archs = [
-    "x86": ["node": "x86_slave", "arch": "amd64"]//,
-    /*
-     * Weird issue with Jenkins remoting, shell steps end up
-     * randomly in java.lang.InterruptedException.
-     * Possibly because of durable-task plugin.
-     *
-     * java.lang.InterruptedException
-     * at java.base/java.lang.Object.wait(Native Method)
-     * at hudson.remoting.Request.call(Request.java:177)
-     * at hudson.remoting.Channel.call(Channel.java:1000)
-     * at hudson.Launcher$RemoteLauncher.launch(Launcher.java:1122)
-     * at hudson.Launcher$ProcStarter.start(Launcher.java:507)
-     * at org.jenkinsci.plugins.durabletask.BourneShellScript.launchWithCookie(BourneShellScript.java:176)
-     * ...
-     *
-     * Temporarily deactivating arm build.
-     */
-    //"arm": ["node": "arm_slave", "arch": "arm64"]
+    "x86": ["node": "x86_slave", "arch": "amd64"],
+    "arm": ["node": "arm_slave", "arch": "arm64"]
   ]
 
   archs.each{ arch, config ->
